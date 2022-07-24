7121ee8b89704c7abc697d33749939787ea4db38
Inna Osipova <rutheva_inna@mail.ru>
2022-07-24 18:23:55 +0300

    Сумма чисел между м и n

---
+++ DZ002/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumNumbers ( int m, int n)
+{
+    if (m == n) return n;
+    return m + SumNumbers (m+1, n);
+}
+
+Console.WriteLine (SumNumbers(2, 4));
