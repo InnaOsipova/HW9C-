51ffb0ead8e3c5ab4c2ed2df32596a98477ffc65
Inna Osipova <rutheva_inna@mail.ru>
2022-07-24 18:15:28 +0300

    выдает все натуральные числа от m до n

---
+++ DZ001/Program.cs
@@ -0,0 +1,16 @@
+﻿ //Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+ string SeriesOfNumbers (int a, int n)
+{
+    if (a == n) 
+    {
+        return n.ToString(); 
+    }
+    else 
+    {
+        return a.ToString() + " " + SeriesOfNumbers(a+1 , n); 
+    }
+}
+
+
+Console.Write(SeriesOfNumbers (1, 10));
